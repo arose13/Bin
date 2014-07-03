09f58d808c9c3d89f88eaa8f8e9753ee30c3f5b1
arose13 <anthony_rose12@hotmail.com>
2014-07-03 14:45:53 -0400

    MainView and layout

--- src/co/binapp/android/views/MainView.java
+++ src/co/binapp/android/views/MainView.java
@@ -2,6 +2,7 @@ package co.binapp.android.views;
 
 import android.os.Bundle;
 
+import co.binapp.android.R;
 import co.binapp.android.activities.ViewActivity;
 
 public class MainView extends ViewActivity {
@@ -9,6 +10,7 @@ public class MainView extends ViewActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_view);
 	}
 	
 }
