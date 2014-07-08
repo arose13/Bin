4e5bece3c41d8622f506a5391866edc1a992b49c
arose13 <anthony_rose12@hotmail.com>
2014-07-08 15:39:37 -0400

    EntryView can now receive external intent payload

--- src/co/binapp/android/views/EntryView.java
+++ src/co/binapp/android/views/EntryView.java
@@ -1,7 +1,16 @@
 package co.binapp.android.views;
 
+import android.os.Bundle;
+
+import co.binapp.android.R;
 import co.binapp.android.activities.ViewActivity;
 
 public class EntryView extends ViewActivity {
-
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.entry_view);
+	}
+	
 }
