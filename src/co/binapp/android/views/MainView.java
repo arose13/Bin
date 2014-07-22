78c21198ce4e88cd5776c60c935c66f21e8c410e
arose13 <anthony_rose12@hotmail.com>
2014-07-22 13:13:58 -0400

    MainView duplications removed

--- src/co/binapp/android/views/MainView.java
+++ src/co/binapp/android/views/MainView.java
@@ -13,33 +13,11 @@ import co.binapp.android.backend.core.CloudBackendFragment.OnListener;
 import co.binapp.android.backend.core.CloudEntity;
 
 public class MainView extends DSConnectedActivity implements OnListener {
-	
-	private static final String PROCESSING_FRAGMENT_TAG = "BACKEND_FRAGMENT";
-	
-	private FragmentManager mFragmentManager;
-	private CloudBackendFragment backendFragment;
-	
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_view);
-		
-		initFrags();
-	}
-
-	private void initFrags() {
-		mFragmentManager = getFragmentManager();
-		FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
-		
-		backendFragment = (CloudBackendFragment) mFragmentManager.findFragmentByTag(PROCESSING_FRAGMENT_TAG);
-		
-		if (backendFragment == null) {
-			backendFragment = new CloudBackendFragment();
-			backendFragment.setRetainInstance(true);
-			fragmentTransaction.add(backendFragment, PROCESSING_FRAGMENT_TAG);
-		}
-		
-		fragmentTransaction.commit();
 	}
 
 	@Override
@@ -51,23 +29,5 @@ public class MainView extends DSConnectedActivity implements OnListener {
 	public void onBroadcastMessageReceived(List<CloudEntity> message) {
 		//TODO: I have no idea what this does
 	}
-	
-//	@Override
-//    public void onCreateFinished() {
-//        listPosts();
-//    }
-//
-//    /**
-//     * Method called via OnListener in {@link CloudBackendFragment}.
-//     */
-//    @Override
-//    public void onBroadcastMessageReceived(List<CloudEntity> l) {
-//        for (CloudEntity e : l) {
-//            String message = (String) e.get(BROADCAST_PROP_MESSAGE);
-//            int duration = Integer.parseInt((String) e.get(BROADCAST_PROP_DURATION));
-//            Toast.makeText(this, message, duration).show();
-//            Log.i(Consts.TAG, "A message was recieved with content: " + message);
-//        }
-//    }
 	
 }
